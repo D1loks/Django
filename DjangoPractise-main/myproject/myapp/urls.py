f8bbffc4dbe8003a13748b1d52c9b358311f3ccf
D1loks <84229264+D1loks@users.noreply.github.com>
2024-10-10 01:38:28 +0300

    Delete DjangoPractise-main directory

--- DjangoPractise-main/myproject/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path("", landing_page, name="landing_page"),
-    path('calculator/', calculator, name='calculator'),
-    path('guess/', guess_the_number, name='guess'),
-    path('hello/', hello, name='hello'),
-]
